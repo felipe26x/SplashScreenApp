1492515a0e365bc7a83addae3b670ab1943afa77
felipe26x <dfvalencia14@misena.edu.co>
2022-06-22 22:00:30 -0500

    nuevo cambio arreglo vista principal

--- app/src/main/java/com/example/splashscreenapp/MainActivity.java
+++ app/src/main/java/com/example/splashscreenapp/MainActivity.java
@@ -4,15 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class  MainActivity extends AppCompatActivity {
     Button ver;
-
+    private TextView textView;
+    private RelativeLayout relativeLayout;
+    private RecyclerView recycler, recycler2;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager LManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +38,26 @@ public class  MainActivity extends AppCompatActivity {
             }
         });
 
+        recycler = findViewById(R.id.reciclador);
+        recycler.setHasFixedSize(true);
 
+
+
+
+        List items = new ArrayList();
+
+        items.add(new Videojuegos(R.drawable.papa, "Fifa 22", 20));
+        items.add(new Videojuegos(R.drawable.platano, "gta 5", 12));
+        items.add(new Videojuegos(R.drawable.cafe, "red dead demption", 32));
+        items.add(new Videojuegos(R.drawable.s, "the last of us 2", 66));
+        items.add(new Videojuegos(R.drawable.zana, "call of duty", 10));
+
+
+        LManager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false);
+        recycler.setLayoutManager(LManager);
+
+        adapter = new VideojuegosAdapter(items);
+        recycler.setAdapter(adapter);
     }
 
     public void fav(View view) {
@@ -47,6 +76,11 @@ public class  MainActivity extends AppCompatActivity {
         startActivity(intent);
 
     }
+
+
+
+
+
 }
 
 
