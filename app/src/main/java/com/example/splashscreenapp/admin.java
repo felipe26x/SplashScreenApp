61c21be6274336864426b99e1b1e47032e68c762
Joseh <jghernandez317@misena.edu.co>
2022-06-23 18:53:33 -0500

    Arreglo de la vista y botones de admin

--- app/src/main/java/com/example/splashscreenapp/admin.java
+++ app/src/main/java/com/example/splashscreenapp/admin.java
@@ -2,7 +2,9 @@ package com.example.splashscreenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class admin extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class admin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
     }
+
+    public void back (View view){
+        Intent intent = new Intent(admin. this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
 }
